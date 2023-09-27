7bfe1e51a94bf167fa54d09ed0bac2093ff462f2
DJ Aaron <koreangeekman@gmail.com>
2023-09-27 12:19:30 -0600

    most of base structure

---
+++ script.js
@@ -0,0 +1,70 @@
+console.log('testy mctesterson')
+
+const menu = [
+  // Category
+  {
+    category: 'Vessels',
+    categoryItems: [
+      {
+        item: 'Waffle Cone',
+        price: 3
+      },
+      {
+        item: 'Waffle Bowl',
+        price: 4
+      },
+      {
+        item: 'Dipped Cone',
+        price: 5
+      }
+    ]
+  },
+  // Category
+  {
+    category: 'Ice Cream',
+    categoryItems: [
+      {
+        item: 'Vanilla',
+        price: 3
+      },
+      {
+        item: 'Chocolate',
+        price: 3
+      },
+      {
+        item: 'Strawberry',
+        price: 3
+      },
+      {
+        item: 'Cookies & Cream',
+        price: 4
+      },
+      {
+        item: 'Mint Chocolate Chip',
+        price: 4
+      },
+      {
+        item: 'Butter Pecan',
+        price: 4
+      }
+    ]
+  },
+  // Category
+  {
+    category: 'Toppings',
+    categoryItems: [
+      {
+        item: 'Sprinkles',
+        price: 2
+      },
+      {
+        item: 'Chocolate Chips',
+        price: 3
+      },
+      {
+        item: 'Gummy Worms',
+        price: 2
+      }
+    ]
+  }
+]
